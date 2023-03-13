f616721ce2d281febcbe92d01fb8a47541d4420a
John <johnmiego@gmail.com>
2023-03-13 16:21:40 -0700

    -Third Commit-

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import requests
+
+API_KEY = "6be7b888210e4bbf8eacafbbd33f2765"
+url = "https://newsapi.org/v2/everything?q=SVB&" \
+      "from=2023-02-13&sortBy=publishedAt&apiKey=" \
+      "6be7b888210e4bbf8eacafbbd33f2765"
+
+request = requests.get(url)
+content = request.json()
+print(content["articles"])
+for article in content["articles"]:
+      print(article["title"])
+      print(article["description"])
